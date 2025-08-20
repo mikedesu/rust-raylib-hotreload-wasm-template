37d027720b7a875a62059b4eb8572507ef65f025
darkmage <165251+mikedesu@users.noreply.github.com>
2025-08-20 17:59:32 -0500

    update

--- game/game.rs
+++ game/game.rs
@@ -1,5 +1,6 @@
 use raylib_wasm::{KeyboardKey as KEY, *};
-use crate::state::State;
+
+// use crate::state::State; // doesn't work
 
 const WINDOW_WIDTH: i32 = 1280;
 const WINDOW_HEIGHT: i32 = 720;
@@ -22,7 +23,6 @@ const TARGET_DST: Rectangle = Rectangle {
 
 const SPEED_DEFAULT: f32 = 850.0;
 const SPEED_BOOSTED: f32 = 1550.0;
-
 
 #[no_mangle]
 pub unsafe fn game_init() -> State {
